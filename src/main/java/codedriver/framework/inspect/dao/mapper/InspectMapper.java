946d8803ef5e6fa902075ccc3d548f949c0a4307
longrf <techsure901>
2021-12-03 19:49:51 +0800

    --story=1005575 --user=龙儒锋 巡检定义 https://www.tapd.cn/54247054/s/1192004

--- src/main/java/codedriver/framework/inspect/dao/mapper/InspectMapper.java
+++ src/main/java/codedriver/framework/inspect/dao/mapper/InspectMapper.java
@@ -1,6 +1,7 @@
 package codedriver.framework.inspect.dao.mapper;
 
-import codedriver.framework.cmdb.dto.ci.CiVo;
+import codedriver.framework.inspect.dto.InspectCiCombopVo;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
