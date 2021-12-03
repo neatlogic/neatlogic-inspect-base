c750d5eba73781e612592c6cb46698b3a8b4d841
longrf <techsure901>
2021-12-03 18:30:14 +0800

    --story=1005575 --user=龙儒锋 巡检定义 https://www.tapd.cn/54247054/s/1192004

---
+++ src/main/java/codedriver/framework/inspect/dto/InspectCiCombopVo.java
@@ -0,0 +1,37 @@
+package codedriver.framework.inspect.dto;
+
+import codedriver.framework.cmdb.dto.ci.CiVo;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+
+public class InspectCiCombopVo extends CiVo {
+
+    @EntityField(name = "combopId", type = ApiParamType.LONG)
+    private Long combopId;
+     @EntityField(name = "combopName", type = ApiParamType.STRING)
+    private String combopName;
+
+    public InspectCiCombopVo(CiVo ciVo) {
+        super.label = ciVo.getLabel();
+    }
+
+    public InspectCiCombopVo() {
+
+    }
+
+    public Long getCombopId() {
+        return combopId;
+    }
+
+    public void setCombopId(Long combopId) {
+        this.combopId = combopId;
+    }
+
+    public String getCombopName() {
+        return combopName;
+    }
+
+    public void setCombopName(String combopName) {
+        this.combopName = combopName;
+    }
+}
