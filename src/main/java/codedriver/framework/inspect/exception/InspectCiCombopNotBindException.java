9731a69bc1bb5c85f0e97200fa4eee381a5f99c5
lvzk <lvzk@techsure.com.cn>
2021-12-03 16:58:14 +0800

    --task=1005621 --user=lvzk 根据ciId或resourceId获取绑定的combopId https://www.tapd.cn/54247054/s/1192343

--- src/main/java/codedriver/framework/inspect/exception/InspectCiCombopNotBindException.java
+++ src/main/java/codedriver/framework/inspect/exception/InspectCiCombopNotBindException.java
@@ -5,6 +5,6 @@ import codedriver.framework.exception.core.ApiRuntimeException;
 public class InspectCiCombopNotBindException extends ApiRuntimeException {
     private static final long serialVersionUID = 7582055639241678278L;
     public InspectCiCombopNotBindException(String name) {
-        super("ci："+name+" 没有绑定组合工具，请确认后重试");
+        super("ci：'"+name+"' 没有绑定组合工具，请确认后重试");
     }
 }
