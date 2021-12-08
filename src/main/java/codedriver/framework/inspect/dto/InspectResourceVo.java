ba662a6acd7e6829619834531c21b480ad6ca3d4
lvzk <lvzk@techsure.com.cn>
2021-12-08 15:15:42 +0800

    update 巡检状态

--- src/main/java/codedriver/framework/inspect/dto/InspectResourceVo.java
+++ src/main/java/codedriver/framework/inspect/dto/InspectResourceVo.java
@@ -8,27 +8,12 @@ package codedriver.framework.inspect.dto;
 import codedriver.framework.autoexec.dto.job.AutoexecJobPhaseNodeVo;
 import codedriver.framework.cmdb.dto.resourcecenter.ResourceVo;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.inspect.constvalue.InspectStatus;
 import codedriver.framework.restful.annotation.EntityField;
-import org.apache.commons.lang3.StringUtils;
 
 public class InspectResourceVo extends ResourceVo {
     private static final long serialVersionUID = -2029368211438672519L;
-    @EntityField(name = "巡检状态Vo", type = ApiParamType.JSONOBJECT)
-    private InspectStatusVo inspectStatusVo;
     @EntityField(name = "作业节点Vo", type = ApiParamType.JSONOBJECT)
     private AutoexecJobPhaseNodeVo jobPhaseNodeVo;
-
-
-    public InspectStatusVo getInspectStatusVo() {
-        if (inspectStatusVo == null && StringUtils.isNotBlank(getInspectStatus())) {
-            InspectStatus inspectStatus = InspectStatus.getInspectStatus(getInspectStatus());
-            if (inspectStatus != null) {
-                return new InspectStatusVo(inspectStatus);
-            }
-        }
-        return inspectStatusVo;
-    }
 
     public AutoexecJobPhaseNodeVo getJobPhaseNodeVo() {
         return jobPhaseNodeVo;
