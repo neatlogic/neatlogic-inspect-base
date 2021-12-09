6d48fdaa40e2945339931a256ae3109421a262d9
longrf <techsure901>
2021-12-09 18:37:17 +0800

    --bug=1008949 --user=龙儒锋 巡检定义-巡检工具规则的所有字段非必填 https://www.tapd.cn/54247054/s/1193971

--- src/main/java/codedriver/framework/inspect/exception/InspectDefLessLevelException.java
+++ src/main/java/codedriver/framework/inspect/exception/InspectDefLessLevelException.java
@@ -4,6 +4,6 @@ import codedriver.framework.exception.core.ApiRuntimeException;
 
 public class InspectDefLessLevelException extends ApiRuntimeException {
     public InspectDefLessLevelException(int i) {
-        super("第" + i + 1 + "规则缺少级别参数");
+        super("第" + (i + 1) + "规则缺少级别参数");
     }
 }
