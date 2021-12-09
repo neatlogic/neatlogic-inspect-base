25933ab816686615de68ff70df85c4de3cbd4ec2
longrf <techsure901>
2021-12-09 18:39:32 +0800

    --bug=1008949 --user=龙儒锋 巡检定义-巡检工具规则的所有字段非必填 https://www.tapd.cn/54247054/s/1193971

--- src/main/java/codedriver/framework/inspect/exception/InspectDefLessRuleException.java
+++ src/main/java/codedriver/framework/inspect/exception/InspectDefLessRuleException.java
@@ -4,6 +4,6 @@ import codedriver.framework.exception.core.ApiRuntimeException;
 
 public class InspectDefLessRuleException extends ApiRuntimeException {
     public InspectDefLessRuleException(int i) {
-        super("第" + (i + 1) + "规则缺少规则参数");
+        super("第" + (i + 1) + "条规则缺少规则参数");
     }
 }
