2d2fef06748a646b6e25f8deabd3b192700d43c6
lvzk <897706680@qq.com>
2023-05-31 17:58:27 +0800

    优化国际化翻译 采用json格式维护

--- src/main/java/neatlogic/framework/inspect/constvalue/AutoexecType.java
+++ src/main/java/neatlogic/framework/inspect/constvalue/AutoexecType.java
@@ -6,7 +6,7 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.util.I18nUtils;
 
 public enum AutoexecType implements IAutoexecType {
-    INSPECT(1L,"INSPECT", "enum.inspect.autoexectype.inspect")
+    INSPECT(1L,"INSPECT", "common.inspection")
     ;
 
     private final Long id;
